e70c5808e86f81e309f0b0cb3a2484e05363bde7
Vishwakant-coder <work.vishwakant.edu@gmail.com>
2024-12-04 21:55:07 +0530

    Day 20

---
+++ Day 20/StringsRotationsofEachOther.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to check if two strings are rotations of each other or not.
+    bool areRotations(string &s1, string &s2) {
+        // Your code here
+        string str = s1+s1;
+        int j=0;
+        if(s1.size()!=s2.size())
+            return false;
+        for(int i=0; i<str.size();i++){
+            if(s2[j] == str[i]) 
+                j++;
+        }
+        return j==s2.size();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1;
+        string s2;
+        cin >> s1 >> s2;
+        Solution sol;
+        int result = sol.areRotations(s1, s2);
+        if (result) {
+            cout << "true" << endl;
+
+        } else {
+            cout << "false" << endl;
+        }
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
